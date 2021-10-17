2151d42eddbaa22437fc1dd83cbf61dbed0b1b4c
yilinshensummer <67626752+yilinshensummer@users.noreply.github.com>
2021-10-17 11:49:57 -0700

    Add js folder

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-west-2_lcWeIjwMf', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '8rndmrbsmfadqqtvafm90srj', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-west-2' // e.g. us-west-2
+    },
+    api: {
+        invokeUrl: 'https://te68s8jurh.execute-api.us-west-2.amazonaws.com/prod' // e.g. https://ttf5alun5d.execute-api.us-west-2.amazonaws.com/prod
+    }
+};
