085af32e4475fe2104914a60571bb9f0e14de543
yilinshensummer <67626752+yilinshensummer@users.noreply.github.com>
2022-07-28 10:43:26 -0700

    Remove info

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-west-2_lcWeIjwMf', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '8rndmrbsmfadqqtvafm90srj', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-west-2' // e.g. us-west-2
+        userPoolId:  // e.g. us-east-2_uXboG5pAb
+        userPoolClientId:  // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: // e.g. us-west-2
     },
     api: {
-        invokeUrl: 'https://te68s8jurh.execute-api.us-west-2.amazonaws.com/prod' // e.g. https://ttf5alun5d.execute-api.us-west-2.amazonaws.com/prod
+        invokeUrl:  // e.g. https://ttf5alun5d.execute-api.us-west-2.amazonaws.com/prod
     }
 };
